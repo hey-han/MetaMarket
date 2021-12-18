a44fc8e0dde858facdc6ff8eabf2d60ef6f13bf0
Zhang Han <ZHAN0486@e.ntu.edu.sg>
2021-12-18 23:05:05 +0800

    Create Fungible_token.sol

---
+++ contracts/Fungible_token.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+// draft version
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract fungible_token is ERC20 {
+    address public admin;
+
+    constructor() ERC20("XXXXX", "XXX") {
+        _mint(msg.sender, 10000*10**18);
+        admin = msg.sender;
+    }
+
+    // do not want a fixed supply
+    function mint(address to, uint amount) external {
+        // access control of minting the token
+        require(msg.sender == admin, "only admin can call this function");
+        _mint(to, amount);
+    }
+
+    // allow people to burn their token
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+}
