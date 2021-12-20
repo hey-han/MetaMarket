0dbcbaaf74e140e63edf89711970ba1cca899207
Zhang Han <ZHAN0486@e.ntu.edu.sg>
2021-12-20 21:57:10 +0800

    Update NFT_market.sol

--- contracts/NFT_market.sol
+++ contracts/NFT_market.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity ^0.8.3;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
