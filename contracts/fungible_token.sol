7ae4fe9c6e0419ec446d805d04ae40302fa53fe2
Joel Lim <joellimjieen@hotmail.com>
2021-12-21 23:04:59 +0800

    added FT modifier and changed imports

--- contracts/fungible_token.sol
+++ contracts/fungible_token.sol
@@ -3,20 +3,25 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract fungible_token is ERC20 {
-    address public admin;
+contract fungible_token is ERC20, ERC20Detailed {
+    address private admin;
 
-    constructor() ERC20("MetaMark", "MetaMark") {
+    constructor () ERC20("MetaMark", "MetaMark") public {
         _mint(msg.sender, 10000*10**18);
         admin = msg.sender;
     }
 
+    modifier onlyOwner {
+        require(msg.sender == admin);
+        _;
+    }
+
     // do not want a fixed supply
-    function mint(address to, uint amount) external {
+    function mint(address to, uint amount) external onlyOwner {
         // access control of minting the token
-        require(msg.sender == admin, "only admin can call this function");
         _mint(to, amount);
     }
 
@@ -24,4 +29,6 @@ contract fungible_token is ERC20 {
     function burn(uint amount) external {
         _burn(msg.sender, amount);
     }
+
+    
 }
