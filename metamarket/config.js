7502fe83aefab3d4bb5e83c86468d1b67c45c03c
Zhang Han <ZHAN0486@e.ntu.edu.sg>
2022-01-04 03:43:15 +0800

    Delete metamarket directory

--- metamarket/config.js
+++
@@ -1,3 +0,0 @@
-export const fungibletokenaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-export const nftmarketaddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
-export const nftaddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
