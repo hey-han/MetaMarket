3becf0a7807f258eed36b447d880aff8c71a092d
Zhang Han <zhanghan4521@gmail.com>
2022-01-12 17:03:36 +0800

    marketplace

--- metamarket/pages/explore/index.tsx
+++ metamarket/pages/explore/index.tsx
@@ -3,10 +3,16 @@ import { experimentalStyled as styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from "web3modal"
 import { WithLogin } from "../../components/AuthModal";
 import ExploreCard from "../../components/ExploreCard";
-import data from "../../data.json";
+
+import {nftmarketaddress, nftaddress} from '../../config'
+import Market from '../../artifacts/contracts/NFT_market.sol/MetaMarket.json'
+import NFT from '../../artifacts/contracts/NFT.sol/NFT.json'
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -16,6 +22,61 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const Explore: NextPage = () => {
+  const [nfts, setNfts] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
+  useEffect(() => {
+    loadNFTs()
+  }, [])
+
+  async function loadNFTs() {
+    /* create a generic provider and query for unsold market items */
+    const provider = new ethers.providers.JsonRpcProvider("https://matic-mumbai.chainstacklabs.com")
+    const nftContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
+    const data = await marketContract.fetchMarketItems()
+
+    /*
+    *  map over items returned from smart contract and format 
+    *  them as well as fetch their token metadata
+    */
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await nftContract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'MetaMark')
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item
+    }))
+    setNfts(items)
+    setLoadingState('loaded') 
+  }
+
+  async function buyNft(nft) {
+    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+
+    /* user will be prompted to pay the asking proces to complete the transaction */
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')   
+    const transaction = await contract.createMarketSale(nftaddress, nft.tokenId, {
+      value: price
+    })
+    await transaction.wait()
+    loadNFTs()
+  }
+
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>)
+
   return (
     <div
       style={{
@@ -23,26 +84,31 @@ const Explore: NextPage = () => {
         marginTop: "60px",
       }}
     >
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
-        >
-          {data.nft.map((data, index) => (
-            <Grid item xs={1} sm={1} md={1} lg={1} xl={1} key={index}>
-              <ExploreCard
-                uid={data.uid}
-                creatorName={data.creatorName}
-                creatorAvatarURL={data.creatorAvatarURL}
-                imageURL={data.imageURL}
-                description={data.description}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      <div className="flex justify-center">
+        <div className="px-4" style={{ maxWidth: '1600px' }}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            {
+              nfts.map((nft,i) => (
+                <div key={i} className="border shadow rounded-xl overflow-hidden">
+                  <img src={nft.image} />
+                  <div className="p-4">
+                    <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
+                    <div style={{ height: '70px', overflow: 'hidden' }}>
+                      <p className="text-gray-400">{nft.description}</p>
+                    </div>
+                  </div>
+                  <div className="p-4 bg-black">
+                    <p className="text-2xl mb-4 font-bold text-white">{nft.price} ETH</p>
+                    <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
     </div>
+    
   );
 };
 
