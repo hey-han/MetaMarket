4597897121fcf9958ae67e6deefc40a95c87c0a9
Zhang Han <zhanghan4521@gmail.com>
2022-01-16 05:00:03 +0800

    sell

--- metamarket/pages/create/index.tsx
+++ metamarket/pages/create/index.tsx
@@ -87,7 +87,7 @@ const Create: NextPage = () => {
             <img className="rounded mt-4" width="350" src={fileUrl} />
           )
         }
-        <button onClick={createNFT} className="font-bold mt-4 bg-sky-400 text-white rounded p-4 shadow-lg">
+        <button onClick={onChange} className="font-bold mt-4 bg-sky-400 text-white rounded p-4 shadow-lg">
           Create New NFT
         </button>
       </div>
--- metamarket/pages/sell/index copy.tsx
+++ metamarket/pages/sell/index copy.tsx
@@ -10,38 +10,18 @@ import {nftmarketaddress, nftaddress} from '../../config'
 import Market from '../../artifacts/contracts/NFT_market.sol/MetaMarket.json'
 import NFT from '../../artifacts/contracts/NFT.sol/NFT.json'
 
-const Create: NextPage = () => {
+const Sell: NextPage = (name, creator, url) => {
   const [fileUrl, setFileUrl] = useState(null)
-  const [formInput, updateFormInput] = useState({ price: '', name: '', creator:'', description: '' })
+  const [formInput, updateFormInput] = useState({ name: '', price: '', creator:'', seller: '', description: '' })
   const router = useRouter()
 
-  /* upload the image to IPFS */
-  async function onChange(e) {
-    const file = e.target.files[0]
-    try {
-      const added = await client.add(
-        file,
-        {
-          progress: (prog) => console.log(`received: ${prog}`)
-        }
-      )
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      setFileUrl(url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-    }
-  }
-
-
   async function createMarket() {
-    const { name, description, price } = formInput
-    if (!name || !description || !price || !fileUrl) return
-    /* first, upload to IPFS */
-    const data = JSON.stringify({
-      name, description, image: fileUrl
-    })
+    const { name, price, creator, seller, description } = formInput
+    if (!name || !price || !creator || !seller || !description || !fileUrl) return
+    const sellData = JSON.stringify({name, price, creator, seller, description, image: fileUrl})
+    /* upload all information to IPFS */
     try {
-      const added = await client.add(data)
+      const added = await client.add(sellData)
       const url = `https://ipfs.infura.io/ipfs/${added.path}`
       /* after file is uploaded to IPFS, pass the URL to save it on Polygon */
       createSale(url)
@@ -56,20 +36,20 @@ const Create: NextPage = () => {
     const provider = new ethers.providers.Web3Provider(connection)    
     const signer = provider.getSigner()
 
-    /* next, create the item */
-    let contract = new ethers.Contract(nftaddress, NFT.abi, signer)
-    let transaction = await contract.createToken(url)
-    let tx = await transaction.wait()
-    let event = tx.events[0]
-    let value = event.args[2]
-    let tokenId = value.toNumber()
-    const price = ethers.utils.parseUnits(formInput.price, 'ether')
+    // /* next, create the item */
+    // let contract = new ethers.Contract(nftaddress, NFT.abi, signer)
+    // let transaction = await contract.createToken(url)
+    // let tx = await transaction.wait()
+    // let event = tx.events[0]
+    // let value = event.args[2]
+    // let tokenId = value.toNumber()
+    // const price = ethers.utils.parseUnits(formInput.price, 'ether')
 
     /* then list the item for sale on the marketplace */
-    contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+    let contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
     let listingPrice = await contract.getListingPrice()
     listingPrice = listingPrice.toString()
-    transaction = await contract.createMarketItem(nftaddress, tokenId, price, { value: listingPrice })
+    let transaction = await contract.createMarketItem(nftaddress, tokenId, price, { value: listingPrice })
     await transaction.wait()
     router.push('/')
   }
@@ -92,23 +72,12 @@ const Create: NextPage = () => {
           className="mt-2 border rounded p-4"
           onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
         />
-        <input
-          type="file"
-          name="New NFT"
-          className="my-4"
-          onChange={onChange}
-        />
-        {
-          fileUrl && (
-            <img className="rounded mt-4" width="350" src={fileUrl} />
-          )
-        }
         <button onClick={createMarket} className="font-bold mt-4 bg-sky-400 text-white rounded p-4 shadow-lg">
-          Create New NFT
+          Sell the NFT
         </button>
       </div>
     </div>
   )
 };
 
-export default Create;
+export default Sell;
